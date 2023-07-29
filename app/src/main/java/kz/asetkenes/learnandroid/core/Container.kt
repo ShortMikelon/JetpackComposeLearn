c5cff031f7aee789f14916644bfff3fec585a4be
ShortMikelon <kenesaset485@gmail.com>
2023-07-29 16:34:27 +0600

    Added Container and its tests.

---
+++ app/src/main/java/kz/asetkenes/learnandroid/core/Container.kt
@@ -0,0 +1,78 @@
+package kz.asetkenes.learnandroid.core
+
+import java.lang.Exception
+import java.lang.IllegalStateException
+
+sealed class Container<out T> {
+
+    fun <R> map(mapper: ((T) -> R)? = null): Container<R> {
+        val innerMapper: ((T) -> R)? = if (mapper == null) {
+            null
+        } else {
+            {
+                mapper(it)
+            }
+        }
+        return innerMap(innerMapper)
+    }
+
+    abstract fun <R> innerMap(mapper: ((T) -> R)? = null): Container<R>
+
+    abstract fun getOrThrowException(): T
+
+    abstract fun getOrNull(): T?
+
+    data class Success<T>(
+        val value: T
+    ) : Container<T>() {
+
+        override fun <R> innerMap(mapper: ((T) -> R)?): Container<R> {
+            if (mapper == null) throw IllegalStateException("Can't map Container.Success without mapper")
+            return try {
+                Success(mapper(value))
+            } catch (ex: Exception) {
+                Error(ex)
+            }
+        }
+
+        override fun getOrThrowException(): T {
+            return value
+        }
+
+        override fun getOrNull(): T? {
+            return value
+        }
+    }
+
+    data class Error(
+        val exception: Exception
+    ) : Container<Nothing>() {
+
+        override fun <R> innerMap(mapper: ((Nothing) -> R)?): Container<R> {
+            return this
+        }
+
+        override fun getOrThrowException(): Nothing {
+            throw exception
+        }
+
+        override fun getOrNull(): Nothing? {
+            return null
+        }
+    }
+
+    object Pending : Container<Nothing>() {
+
+        override fun <R> innerMap(mapper: ((Nothing) -> R)?): Container<R> {
+            return this
+        }
+
+        override fun getOrThrowException(): Nothing {
+            throw IllegalStateException("Container is Pending")
+        }
+
+        override fun getOrNull(): Nothing? {
+            return null
+        }
+    }
+}
---
+++ app/src/test/java/kz/asetkenes/learnandroid/core/ContainerTest.kt
@@ -0,0 +1,101 @@
+package kz.asetkenes.learnandroid.core
+
+import kz.asetkenes.learnandroid.core.Container.Error
+import kz.asetkenes.learnandroid.core.Container.Pending
+import kz.asetkenes.learnandroid.core.Container.Success
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertSame
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class ContainerTest {
+
+    @Test(expected = Exception::class)
+    fun getOrThrowExceptionThrowsExceptionForErrorContainer() {
+        val error = Error(Exception())
+
+        error.getOrThrowException()
+
+        wellDone()
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun getOrThrowExceptionThrowsNothingForPendingContainer() {
+        val pending = Pending
+
+        pending.getOrThrowException()
+
+        wellDone()
+    }
+
+    @Test
+    fun getOrThrowExceptionReturnValueForSuccessContainer() {
+        val success = Success("value")
+
+        val successValue = success.getOrThrowException()
+
+        assertEquals("value", successValue)
+    }
+
+    @Test
+    fun getOrNullReturnsNullForNonSuccessContainer() {
+        val error = Error(Exception())
+        val pending = Pending
+
+        val errorValue = error.getOrNull()
+        val pendingValue = pending.getOrNull()
+
+        assertNull(errorValue)
+        assertNull(pendingValue)
+    }
+
+    @Test
+    fun getOrNullReturnsValueForSuccessContainer() {
+        val success = Success("value")
+
+        val successValue = success.getOrNull()
+
+        assertEquals("value", successValue)
+    }
+
+    @Test
+    fun mapNonSuccessContainerMapping() {
+        val exception = Exception()
+        val error = Error(exception)
+        val pending = Pending
+        val success = Success("value")
+
+        val mappedErrorResult = error.map<Int>()
+        val mappedPendingResult = pending.map<Int>()
+        val mappedSuccessResult = success.map<Int>()
+
+        assertTrue(mappedErrorResult is Error)
+        assertTrue(mappedPendingResult is Pending)
+        assertTrue(mappedSuccessResult is Success)
+        assertSame(exception, (mappedErrorResult as Error).exception)
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun mapWithoutMapperCantConvertSuccessContainer() {
+        val container = Success("value")
+
+        container.map<Int>()
+
+        wellDone()
+    }
+
+    @Test
+    fun mapWithMapperConvertSuccessToSuccess() {
+        val container = Success(321)
+
+        val mappedContainer = container.map { it.toLong() }
+
+        assertTrue(mappedContainer is Success)
+        assertEquals(321L, (mappedContainer as Success).value)
+    }
+
+    private  fun wellDone() {
+
+    }
+}
